15883089545664bfa93304a876c3e8f35ab0ce87
joy <letimjoy7@gmail.com>
2022-12-05 22:41:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let newName = [];
+    for(let k=0; k<name.length; k++) {
+        let message = `Thank you, ${name[k]}, for the wonderful ${event} gift!`
+        newName.push(message);
+    }
+    return newName
+}
+
+function countDown(num){
+    for(let i=num ; i>=0; i--){
+        console.log(i);
+    }
+}
+console.log(countDown(10));
